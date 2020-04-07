718c501dc07be4792e0b6f77d90724b73ccef72b
yuvtorres <yuvtorres@gmail.com>
2020-04-07 18:54:47 +0200

    lab-numpy

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,10 +1,12 @@
 #1. Import the NUMPY package under the name np.
-
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
+print(f" Mi versión de numpy es: {np.version.version}")
 
-
+"""
+ Mi versión de numpy es: 1.17.4
+"""
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
